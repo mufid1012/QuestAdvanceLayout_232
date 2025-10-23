8aa98703ba727db3d7a17a7d58f34600899f3e68
mufid1012 <mufidghibran1012@gmail.com>
2025-10-23 09:18:33 +0700

    Membenarkan MainActivity

--- app/src/main/java/com/example/myuipertama/MainActivity.kt
+++ app/src/main/java/com/example/myuipertama/MainActivity.kt
@@ -3,12 +3,16 @@ package com.example.myuipertama
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.Surface
+import androidx.compose.material3.MaterialTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ActivitasPertama()
+            Surface(color = MaterialTheme.colorScheme.background) {
+                ActivitasPertama() // ✅ tampilkan composable kamu di sini
+            }
         }
     }
 }
